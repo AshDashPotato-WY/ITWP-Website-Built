5e66bb1f46d95cfe1566f1677e0650748d438033
AshDashPotato-WY <ashleyweiyu@yahoo.com>
2023-10-05 01:15:43 -0400

    first commit

---
+++ itwp1000/project2/js/footer.js
@@ -0,0 +1,6 @@
+// Javascript document
+// This is Javascript to configure secondary navigation in the footer
+
+document.write("<div>");
+document.write("<a href='project2home.html'>Home</a> | <a href='zodiacanimals.html'>Zodiac Animals</a> | <a href='foods.html'>Foods</a> | <a href='tranditions.html'>Tranditions</a> | <a href='lanternfestival.html'>Lantern Festival</a> | <a href='../home.htm'>Course Homepage</a>");
+document.write("</div>");
---
+++ itwp1000/project4/js/footer.js
@@ -0,0 +1,7 @@
+// Javascript document
+// This is Javascript to configure secondary navigation in the footer
+
+document.write("<header><h1>links</h1></header>");
+document.write("<ul>");
+document.write("<li><a href='index.html'>Home</a></li><li><a href='rules.html'>Rules</a></li><li><a href='category.html'>Categories</a></li><li><a href='gallery.html'>Winners Gallery</a></li><li><a href='submission.html'>Submission</a></li><li><a href='vote.html'>Vote</a></li><li><a href='sitemap.xml'>Sitemap</a></li>");
+document.write("</ul>");
